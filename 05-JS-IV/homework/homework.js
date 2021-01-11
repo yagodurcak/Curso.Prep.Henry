5b05176bd8b80816b08ecf961a7881f2d675171d
yagodurcak <mauridurcak@gmail.com>
2021-01-11 18:09:03 -0300

    05-js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { resta } = require("../../02-JS-I/homework/homework");
+
 function crearGato(nombre, edad) {
   // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
@@ -32,26 +34,38 @@ function invocarMetodo(objeto, metodo) {
   // Nada necesita ser devuelto ("returned")
   // Tu código:
   
-  objeto[método]();
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
-}
+  objetoMisterioso.multiplica = function() {
+    return this.numeroMisterioso * 5
+  }
+  return objetoMisterioso["multiplica"]();
+  }
 
 function eliminarPropiedad(objeto, propiedad) {
   // Elimina la propiedad "propiedad" de "objeto"
   // Devuelve el objeto
   // Tu código:
+  delete objeto[propiedad];
+  return objeto;
+
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  var usuario  = {
+    nombre: nombre,
+    email: email,
+    password:  password
+  }
+  return usuario;
 
 }
 
@@ -59,6 +73,11 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario.email != null ){
+    return true;
+    
+
+  }else {return false}
 }
 
 
@@ -67,6 +86,9 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (propiedad in objeto){
+    return true;
+  } else {return false};
 }
 
 function verificarPassword(usuario, password) {
@@ -74,12 +96,17 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if (usuario.password == password){
+    return true;
+  } else {return false};
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword
+  return usuario
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -87,6 +114,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -95,6 +124,13 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+ for (i=0; i < usuarios.length; i++){
+
+  usuarios[i].esPremium = true;
+
+
+ }
+ return usuarios
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -104,6 +140,13 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  suma = 0;
+  for (i=0; i < usuario.posts.length; i++){
+
+    suma = suma + usuario.posts[i].likes;
+  
+     }
+   return suma
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -116,7 +159,13 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function(){
+    //seusa  this porque es un metodo dentro de un obbjeto/////
+    calculo= this.precio * this.porcentajeDeDescuento;
+    restas = this.precio -  calculo;
+    return restas
+  }
+  return producto
 }
 
 // No modificar nada debajo de esta línea
